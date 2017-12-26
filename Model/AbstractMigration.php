116d35d863a6edb4f527f38568030d317ac564e4
de Longeaux <etienne.delongeaux@gmail.com>
2017-12-26 16:04:03 +0100

    #000 improve abstract migration model

--- Model/AbstractMigration.php
+++ Model/AbstractMigration.php
@@ -1,22 +1,7 @@
 <?php
-/**
- * This file is part of the <Migration> project.
- *
- * @category   Migration
- * @package    Model
- * @abstract
- * @author     Etienne de Longeaux <etienne.delongeaux@gmail.com>
- * @copyright  2015 PI6GROUPE
- * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
- * @version    2.3
- * @link       http://opensource.org/licenses/gpl-license.php
- * @since      2015-02-16
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 namespace Sfynx\MigrationBundle\Model;
 
+use Exception;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\HelperInterface;
@@ -36,23 +21,26 @@ use Symfony\Component\Console\Helper\HelperInterface;
  */
 abstract class AbstractMigration
 {
-    /**
-     * @var ContainerInterface
-     */
+    /** @var ContainerInterface */
     protected $container;
-
-    /**
-     * @var OutputInterface
-     */
+    /** @var OutputInterface */
     protected $output;
+    /** @var DialogHelper */
+    protected $dialog;
+    /** @var array */
+    protected $em = [];
 
     /**
-     * @var DialogHelper
+     * AbstractMigration constructor.
+     * @param ContainerInterface $container
+     * @param OutputInterface $output
+     * @param HelperInterface $dialog
      */
-    protected $dialog;
-
-    public function __construct(ContainerInterface $container, OutputInterface $output, HelperInterface $dialog)
-    {
+    public function __construct(
+        ContainerInterface $container,
+        OutputInterface $output,
+        HelperInterface $dialog
+    ) {
         $this->container = $container;
         $this->output    = $output;
         $this->dialog    = $dialog;
@@ -64,23 +52,39 @@ abstract class AbstractMigration
         }
     }
 
+    /**
+     * @return bool
+     */
     protected function test()
     {
         return true;
     }
 
+    /**
+     *
+     */
     protected function PreUp()
     {
-        // do something
+        $this->beginTransaction();
     }
 
+    /**
+     * @return mixed
+     */
     abstract protected function Up();
 
+    /**
+     *
+     */
     protected function PostUp()
     {
-        // do something
+        $this->commitTransaction();
     }
 
+    /**
+     * @param $msg
+     * @param null $test
+     */
     protected function log($msg, $test = null)
     {
         if (null === $test) {
@@ -89,6 +93,61 @@ abstract class AbstractMigration
             $this->output->writeln("  $msg <info>[OK]</info>");
         } else {
             $this->output->writeln("  $msg <error>[KO]</error>");
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTransactionManagers(): array
+    {
+        return null;
+    }
+
+    /**
+     * begin transactions of all command repository manager
+     * @return bool
+     */
+    protected function beginTransaction()
+    {
+        if (null === $this->getTransactionManagers()) {
+            return false;
+        }
+
+        foreach ($this->getTransactionManagers() as $k => $em) {
+            $this->em[$k] = $em;
+            $this->em[$k]->getConnection()->beginTransaction();
+        }
+
+        return true;
+    }
+
+    /**
+     * Commit transactions of all command repository manager
+     * @return bool
+     * @throws Exception
+     */
+    protected function commitTransaction()
+    {
+        if (null === $this->getTransactionManagers()) {
+            return false;
+        }
+
+        try {
+            // Try and commit the transaction
+            foreach ($this->getTransactionManagers() as $k => $em) {
+                $this->em[$k]->flush();
+                $this->em[$k]->commit();
+            }
+
+            return true;
+        } catch (Exception $e) {
+            // Rollback the failed transaction attempt
+            foreach ($this->getTransactionManagers() as $k => $em) {
+                $this->em[$k]->rollback();
+                $this->em[$k]->close();
+            }
+            throw new Exception($e->getMessage());
         }
     }
 }
